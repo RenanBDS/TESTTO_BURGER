f1a295ff27bea8cb5f4d268bf550bf5ae5bb8f3a
RenanBDS <renanbsantos23@gmail.com>
2024-11-30 18:50:14 -0300

    Feito o Efeito Click
    Finalizar alguns JS para animação com o usuário

--- compra.js
+++ compra.js
@@ -124,3 +124,13 @@ letras_compras.forEach((button) => {
     button.classList.toggle("verde");
   });
 });
+
+var dropdown = document.querySelector(".dropdown");
+dropdown.addEventListener("click", function () {
+  var dropdownContenT = document.querySelector(".dropdown-content");
+  if (dropdownContenT.style.display === "block") {
+    dropdownContenT.style.display = "none";
+  } else {
+    dropdownContenT.style.display = "block";
+  }
+});
